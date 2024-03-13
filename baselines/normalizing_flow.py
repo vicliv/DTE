0c7b26d5afdcaee92c37ea74d13454e42702c5f3
Victor Livernoche <78241611+vicliv@users.noreply.github.com>
2024-03-12 22:40:49 -0400

    Update normalizing_flow.py

--- baselines/normalizing_flow.py
+++ baselines/normalizing_flow.py
@@ -1,3 +1,8 @@
+"""
+On Diffusion Modeling for Anomaly Detection - Diffusion Time Estimation
+@Author: Victor Livernoche <vlivernoche@gmail.com>
+"""
+
 import numpy as np
 import torch
 import torch.nn as nn
